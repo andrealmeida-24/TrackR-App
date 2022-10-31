ace2d6feb43c8d2f5580e1a8b5e5907d25a7beba
André Almeida <102152757+andrealmeida-24@users.noreply.github.com>
2022-10-31 10:23:53 +0000

    Add files via upload
    Version update: 
    -Large and small screens view and functionality;
    -Landing page final version;
    -Geolocation API enhanced

--- script.js
+++ script.js
@@ -62,6 +62,8 @@ class Surfing extends Workout {
 
 //////////////////////////////////////////////
 // APP ARCHITECTURE
+const fullWidthContainer = document.querySelector('.full-width-container');
+
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -77,10 +79,24 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+const clearAllWorkouts = document.querySelector('.clear-all--txt');
+const modalParent = document.querySelector('.modal-parent');
+const closeModalBtn = document.querySelector('.X');
+const modalWindowTxt = document.querySelector('.modal-window--txt');
+
+const deleteDesableCloseBtn = document.querySelector('.delete-desable-btn');
+const deleteActiveBtn = document.querySelector('.delete-active-btn');
+
+const moveToCurrentBtn = document.querySelector('.current-location--txt');
+
+const backHomeBtn = document.querySelector('.back-home-btn');
+
 class App {
   #map;
   #mapEvent;
   #workouts = [];
+  #latitude;
+  #longitude;
 
   constructor() {
     //get user's position
@@ -95,6 +111,42 @@ class App {
     inputType.addEventListener('change', this._changingFieldsVisibility);
 
     containerWorkouts.addEventListener('click', this._moveToPopUp.bind(this));
+
+    //Opening modal when clear all workouts
+    clearAllWorkouts.addEventListener(
+      'click',
+      this._openModalWindow.bind(this)
+    );
+
+    //Close modal with mouse click
+    closeModalBtn.addEventListener('click', this._closeModalWindow.bind(this));
+
+    //Close modal with keyboard event
+    document.body.addEventListener(
+      'keyup',
+      this._closeModalWindowEsc.bind(this)
+    );
+
+    //Close modal with button inside modal
+    deleteDesableCloseBtn.addEventListener(
+      'click',
+      this._closeModalWindow.bind(this)
+    );
+
+    //Delete all workouts
+    deleteActiveBtn.addEventListener(
+      'click',
+      this._deleteAllWorkoutsInArray.bind(this)
+    );
+
+    //Move to current location btn
+    moveToCurrentBtn.addEventListener(
+      'click',
+      this._moveToCurrentLocation.bind(this)
+    );
+
+    // window mobile ? not working
+    //this._checkWindowSource();
   }
 
   //geolocation API -> get position, if true = function loadMap, if false function alert
@@ -103,7 +155,7 @@ class App {
       navigator.geolocation.getCurrentPosition(
         this._loadMap.bind(this),
         function () {
-          alert("Couldn't get your position!");
+          alert("Couldn't get your current position!");
         }
       );
   }
@@ -112,6 +164,9 @@ class App {
   _loadMap(position) {
     const { latitude } = position.coords;
     const { longitude } = position.coords;
+
+    this.#latitude = latitude;
+    this.#longitude = longitude;
 
     const coords = [latitude, longitude];
 
@@ -389,6 +444,80 @@ class App {
     this.#workouts.forEach(w => {
       this._renderWorkout(w);
     });
+  }
+
+  _openModalWindow() {
+    modalParent.style.display = 'block';
+    fullWidthContainer.style.filter = 'blur(8px)';
+
+    if (this.#workouts.length !== 0) {
+      modalWindowTxt.textContent =
+        'Are you sure? By clicking on DELETE, all the workouts saved will be permanently lost.';
+
+      deleteActiveBtn.classList.remove('hidden-to-hide');
+    } else {
+      modalWindowTxt.textContent =
+        'Currently you have 0 workouts logged, try to log some workouts first!';
+      deleteActiveBtn.classList.add('hidden-to-hide');
+    }
+  }
+
+  _closeModalWindow() {
+    modalParent.style.display = 'none';
+    fullWidthContainer.style.filter = 'blur(0px)';
+  }
+
+  _closeModalWindowEsc(e) {
+    if (e.keyCode === 27) {
+      this._closeModalWindow();
+    }
+  }
+
+  _deleteAllWorkoutsInArray() {
+    //clear array
+    this.#workouts.length = 0;
+
+    //clear local storage
+    localStorage.clear();
+    console.log(this.#workouts);
+    this._closeModalWindow();
+    location.reload();
+  }
+
+  _moveToCurrentLocation() {
+    const coords = [this.#latitude, this.#longitude];
+
+    this.#map.setView(coords, 17, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+  }
+
+  // not being used
+  _checkWindowSource() {
+    let check = false;
+    (function (a) {
+      if (
+        /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+          a
+        ) ||
+        /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+          a.substr(0, 4)
+        )
+      )
+        check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    if (check) {
+      this._openModalWindow();
+      modalWindowTxt.textContent =
+        'Sorry 😕  this App is not supported by mobile browsers, try on your laptop!';
+      closeModalBtn.style.display = 'none';
+      deleteDesableCloseBtn.style.display = 'none';
+      backHomeBtn.classList.remove('hidden-to-hide');
+    }
+    return check;
   }
 }
 
